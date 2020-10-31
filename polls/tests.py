4c59fdd83162d60cf5f987694ef2f12bf99f3584
rfujisaki <sax.friza2421db@gmail.com>
2020-10-31 16:03:23 +0900

    to Tutorial5

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,96 @@
 from django.test import TestCase
+import datetime
+
+from django.utils import timezone
+from .models import Question
+from django.urls import reverse
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexIveeweTests(TestCase):
+    def test_no_questions(self):
+        #questionが存在しなかったら適切なメッセージを表示させる
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_questionn(self):
+        #pub_dateが過去のquestionはindexページに表示される
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_future_question(self):
+        #pub_dateが将来の日付のquestionはindexに表示されない
+        create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'],[])
+
+    def test_future_question_and_past_question(self):
+        #pub_dateが将来の物と過去の物が両方存在したら、過去の方だけ表示させる
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_two_past_question(self):
+        #indexページには複数のquestionが表示される
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
+
+class QuestionDatailViewTests(TestCase):
+    def test_future_question(self):
+
+        #将来のquestionは404を返す
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response  =self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        #過去ものもは普通に表示される
+        past_question = create_question(question_text="Past Question", days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        #将来の日付はfalseを返すべき
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+        #ここのselfは何?
+
+    def test_was_publisehd_recently_with_old_question(self):
+        #1日以上経過してたらout 
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recently_question(self):
+        #経過時間が1日未満ならOK
+        #今から23時間59分59秒分の時間を引いてる(昨日から1日経ってない)
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
 
 # Create your tests here.
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,72 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+"""
 def index(request): 
-    return HttpResponse("hello")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+        #辞書 templateのlastest_question_listの部分に、ここに書かれているlatest_question_listを入れている？
+    }
+    return render(request, 'polls/index.html', context)
 
+"""
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        #まだpublishedされていないquesitonを排除
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+
+"""
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+"""
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+"""
+def results(request, question_id):
+    response = "You're looking at the results of question {}." .format(question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+"""
+
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        #POSTデータにchoiceがなかったら、KeyErrorを送出する
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        #ここの引数はURLにquestion.idが入ってるからその分？
 # Create your views here.
